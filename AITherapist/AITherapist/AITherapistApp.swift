07f8cede80eaa541b66ea785200c2e6dd6e815cc
Shirin-Yan <shirin-yan@ShirinMBP.local>
2024-08-03 20:55:16 +0500

    Project created

---
+++ AITherapist/AITherapist/AITherapistApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITherapistApp.swift
+//  AITherapist
+//
+//  Created by Shirin-Yan on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AITherapistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
