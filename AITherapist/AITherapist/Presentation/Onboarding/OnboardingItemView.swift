ace9ca27084a8c7fbd930fd289fc64481dc51ad2
Shirin-Yan <shirin-yan@ShirinMBP.local>
2024-08-07 11:08:18 +0500

    Onboarding ui done

---
+++ AITherapist/AITherapist/Presentation/Onboarding/OnboardingItemView.swift
@@ -0,0 +1,33 @@
+//
+//  OnboardingItemView.swift
+//  AITherapist
+//
+//  Created by Shirin-Yan on 07.08.2024.
+//
+
+import SwiftUI
+
+struct OnboardingItemView: View {
+    
+    var img = ""
+    var title = ""
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(img)
+                .resizable()
+                .scaledToFit()
+            
+            Text(LocalizedStringKey(title))
+                .font(.inter(28, fontWeight: .regular))
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.textColor)
+        }.padding(.horizontal, 50)
+
+    }
+}
+
+#Preview {
+    OnboardingItemView()
+}
---
+++ AITherapist/AITherapist/Presentation/Onboarding/OnboardingView.swift
@@ -0,0 +1,69 @@
+//
+//  OnboardingView.swift
+//  AITherapist
+//
+//  Created by Shirin-Yan on 07.08.2024.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    @State var selectedInd = 0
+
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("AI Therapist")
+                .font(.inter(42, fontWeight: .regular))
+                .foregroundColor(.textColor)
+            
+            TabView(selection: $selectedInd, content:  {
+                
+                OnboardingItemView(img: "onboard-1", title: "onboard_1")
+                    .tag(0)
+                
+                OnboardingItemView(img: "onboard-2", title: "onboard_2")
+                    .tag(1)
+                
+                OnboardingItemView(img: "onboard-3", title: "onboard_3")
+                    .tag(2)
+            }).tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            
+            Button {
+                if selectedInd > 1 {
+                    skipOnboarding()
+                } else {
+                    withAnimation {
+                        selectedInd += 1
+                    }
+                }
+            } label: {
+                Text("continue".uppercased())
+                    .foregroundColor(.textColor)
+                    .font(.inter(20, fontWeight: .regular))
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+                    .background(Color.accentColor)
+                    .cornerRadius(10)
+            }
+            
+            Button {
+                skipOnboarding()
+            } label: {
+                Text("skip".uppercased())
+                    .foregroundColor(.textColor)
+                    .font(.inter(20, fontWeight: .regular))
+            }
+        }.padding(.vertical, 20)
+    }
+    
+    func skipOnboarding(){
+        
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
